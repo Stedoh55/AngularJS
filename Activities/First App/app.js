e3311e30dae773e22d73696a1d5b9dd0398c1847
Steven Simoni <157221633+Stedoh55@users.noreply.github.com>
2024-06-28 21:49:11 +0300

    The first Angular Application to input the data

---
+++ Activities/First App/app.js
@@ -0,0 +1,15 @@
+(function () {
+  'use strict';
+  angular.module('Steven angular app', [])
+  .controller('InsertName', function functionName($scope) {
+    $scope.devName = 'Steven Simoni'
+    $scope.sayHI = function hello() {
+      return 'Hello World!';
+      }
+    $scope.nameInput = ''
+  }); //End of Contoller -- InsertName --
+
+
+
+
+})(); //Close Tag of the IIFE
