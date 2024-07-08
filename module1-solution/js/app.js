318986b562b0f09c8d3539a3b4389d124fd28ba4
Steven Simoni <157221633+Stedoh55@users.noreply.github.com>
2024-07-08 21:07:54 +0300

    The modifications on the changing of styles from direct DOM call to Interpolating by AngularJS

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -12,13 +12,13 @@
       // When 'Check if Too Much' button is Clicked
       $scope.countItems = function () {
         const word = $scope.foodInputs.split(',');      //Calculating number of Commas
-        document.querySelector('#messageField').style.color = 'green';      //Changing message colour to green (success)
-        document.querySelector('#lunch-menu').style.borderColor = 'green';     //Changing border colour to green(success)
+        $scope.colorMessage = 'green';      //Changing message colour to green (success)
+        $scope.colorBorder = 'green';     //Changing border colour to green(success)
 
           if ($scope.foodInputs.length === 0) {      //When there is no user input
               $scope.foodMessage = 'Please enter data first';
-              document.querySelector('#messageField').style.color = 'red';           //Changing message color to red (error)
-              document.querySelector('#lunch-menu').style.borderColor = 'red';        //Changing the border colour to red (error)
+              $scope.colorMessage = 'red';           //Changing message color to red (error)
+              $scope.colorBorder = 'red';        //Changing the border colour to red (error)
 
 
             } else if (word.length <= 3){      //When there are less three or less commas
@@ -31,7 +31,7 @@
       }      //End of Count Items function
 
       $scope.tryAgain = function () {      //This function reset the effects of countitems function on blurring the mouse
-        document.querySelector('#lunch-menu').style.borderColor = ''; //Reseting Border color
+        $scope.colorBorder = ''; //Reseting Border color
         $scope.foodMessage = '';      //Deletes the message
                 }
 
